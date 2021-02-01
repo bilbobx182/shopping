317caa21d42ea0d7b0816b6b26a47eaf81c2e175
Ciaran O Nuallain <onuallainc@gmail.com>
2021-02-01 21:32:46 +0000

    Supervalu class added

--- common.py
+++ common.py
@@ -1,3 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+
 def remove_after_keyword(data, key):
     return str(data.split(key)[0] if key in data else data)
 
@@ -6,3 +9,10 @@ def replace_ownbrand(data,brand):
 
 def remove_currency(data):
     return data.replace('€',"")
+
+def generate_insert(catagory,item,shop,data):
+    return (f'INSERT into product values ({catagory},{item},{shop},{data[1].strip()},{data[2].strip()});')
+
+def perform_request(url):
+    result = requests.get(url)
+    return BeautifulSoup(result.content, "html.parser")
---
+++ supervalu.py
@@ -0,0 +1,26 @@
+from common import replace_ownbrand,remove_currency,generate_insert,perform_request
+
+class Supervalu():
+
+    def __init__(self,catagories):
+        self._catagories = catagories
+
+    def get_supervalu_products(self):
+        for catagory in self._catagories:
+            return_list = []
+            soup = perform_request(f'https://shop.supervalu.ie/shopping/search/allaisles?q={catagory}')
+            for product in soup.find_all("div", {"id": "search-all-aisles-listings-view"})[0].contents:
+                try:
+                    if "LISTING-MID-0" not in product.text:
+                        data = remove_currency(replace_ownbrand(
+                            product.text.split("\n\n\n\n\n\n\n")[3].replace("\n", "").replace("                ",
+                                                                                              "").strip(),
+                            "supervalu")).split("    ")
+                        item = (data[0])
+                        sql = generate_insert(catagory, item, 'SuperValu', data)
+                        return_list.append(sql)
+                except AttributeError as e:
+                    continue
+                except IndexError as e:
+                    continue
+            return return_list
