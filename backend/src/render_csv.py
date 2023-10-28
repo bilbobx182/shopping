b0d5b049cbea4730c5dfafad4dabfaa38c6ea3b9
Ciaran O Nuallain <onuallainc@gmail.com>
2023-10-28 14:13:47 +0100

    Support meta or product

--- backend/src/render_csv.py
+++ backend/src/render_csv.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 import sys
 
 from Dunnes import Dunnes
@@ -6,7 +7,6 @@ from Tesco import Tesco
 from Aldi import Aldi
 from Supervalu import Supervalu
 from common import stats
-
 
 good = ['carrots', 'brocolli', 'parsnip', 'peas', 'soup', 'salad',
         'coriander', 'orange', 'apple', 'pear', 'onions', 'pineapple', 'pepper',
@@ -28,14 +28,13 @@ tesco = Tesco()
 aldi = Aldi()
 super = Supervalu()
 
-
 current_datetime = datetime.datetime.now()
 formatted_date_time = current_datetime.strftime("%Y%m%d_%H%M%S")
 
 # Convert the formatted date and time to a string
 now = str(formatted_date_time)
 
-
+STATS = os.getenv("STATS", True)
 FILENAME = f"{now}_shopping.csv"
 with open(FILENAME, 'w') as sys.stdout:
     print(f"Company, category, sampleSize ,mean, median , mode")
@@ -46,9 +45,14 @@ with open(FILENAME, 'w') as sys.stdout:
             dunnes_products = dunnes.search_product(product)
             tesco_prod = tesco.search_product(product)
             try:
-                stats(dunnes_products['meta'],'dunnes',product)
-                stats(aldi_prod['meta'],'aldi',product)
-                stats(super_prod['meta'],'supervalu',product)
-                stats(tesco_prod['meta'],'tesco',product)
+                if STATS:
+                    stats(dunnes_products['meta'], 'dunnes', product)
+                    stats(aldi_prod['meta'], 'aldi', product)
+                    stats(super_prod['meta'], 'supervalu', product)
+                    stats(tesco_prod['meta'], 'tesco', product)
+                else:
+                    for result in [*aldi_prod['products'], *dunnes_products['products'], *tesco_prod['products'],
+                                    *super_prod['products']]:
+                        print(result)
             except Exception as e:
                 print()
